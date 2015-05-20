89debc917cac88a0389f2b2bc94873dd1717ab5d
Louis van Geldrop <louis@vangeldrop.com>
2015-05-20 20:35:35 +0200

    iapl monadic naar git

---
+++ APL/iapl monadic.ts
@@ -0,0 +1,78 @@
+﻿//#region "Monadic Scalar Numeric Primitive"
+
+interface Number {
+    //rotate<T>(array: T[])
+    //deal(omega: number, alpha?: number): number[]
+   
+    sign: number
+    identity: number
+    negate: number
+    reciprocal: number
+    exponential: number
+    ln: number
+    pi: number
+    factorial: number
+    roll: number
+    magnitude: number
+    ceiling: number
+    floor: number
+    same: number
+    enclose: any
+    indexGenerator: number[]
+    ravel: number[]
+    format: string
+}
+
+
+
+interface NumberConstructor {
+    iota<T>(length?: T): number[]
+    rotate<T>(array: T[])
+    sign(): number
+    identity(): number
+    negate(): number
+    reciprocal(): number
+    exponential(): number
+    ln(): number
+    pi(): number
+    factorial(): number
+    roll(): number
+    magnitude(): number
+    ceiling(): number
+    floor(): number
+    same(): number
+}
+
+//#endregion
+
+//#region "Monadic Scalar Vector Primitive"
+
+interface Array<T> {
+    sign: T[]
+    identity: T[]
+    gradeUp: T[]
+    gradeUpQS: T[]
+    gradeDown: T[]
+    negate: T[]
+    reciprocal: T[]
+    exponential: T[]
+    ln: T[]
+    pi: T[]
+    factorial: T[]
+    roll: T[]
+    magnitude: T[]
+    ceiling: T[]
+    floor: T[]
+    same: T[]
+    enclose: any
+    indexGenerator: T[]
+    ravel: T[]
+    transpose: T[]
+    format: string
+}
+//#endregion
+
+
+
+
+
