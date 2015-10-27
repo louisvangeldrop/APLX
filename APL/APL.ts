35f3042815966b075829f05e200bce426247dc31
Louis van Geldrop <louis@vangeldrop.com>
2015-10-27 22:44:10 +0100

    Interfaces naar APL.ts

--- APL/APL.ts
+++ APL/APL.ts
@@ -1,4 +1,135 @@
-﻿namespace Monadic {
+﻿//#region "Monadic Scalar Numeric Primitive"
+
+interface Number {
+    //rotate<T>(array: T[])
+    //deal(omega: number, alpha?: number): number[]
+   
+    signum: number
+    identity: number
+    negate: number
+    reciprocal: number
+    exponential: number
+    ln: number
+    pi: number
+    factorial: number
+    roll: number
+    magnitude: number
+    ceiling: number
+    floor: number
+    same: number
+    enclose: any
+    indexGenerator: number[]
+    ravel: number[]
+    format: string
+}
+
+
+
+interface NumberConstructor {
+    iota<T>(length?: T): number[]
+    rotate<T>(array: T[])
+    signum(): number
+    identity(): number
+    negate(): number
+    reciprocal(): number
+    exponential(): number
+    ln(): number
+    pi(): number
+    factorial(): number
+    roll(): number
+    magnitude(): number
+    ceiling(): number
+    floor(): number
+    same(): number
+}
+
+//#endregion
+
+//#region "Monadic Scalar Vector Primitive"
+
+interface Array<T> {
+    signum: T[]
+    identity: T[]
+    gradeUp: T[]
+    gradeUpQS: T[]
+    gradeDown: T[]
+    negate: T[]
+    reciprocal: T[]
+    exponential: T[]
+    ln: T[]
+    pi: T[]
+    factorial: T[]
+    roll: T[]
+    magnitude: T[]
+    ceiling: T[]
+    floor: T[]
+    same: T[]
+    enclose: any
+    indexGenerator: T[]
+    ravel: T[]
+    transpose: T[]
+    format: string
+}
+//#endregion
+
+//#region "Monadic Scalar Primitive"
+
+interface Number {
+    rotate<T>(array: T[])
+    deal(omega: number): number[]
+
+}
+
+//#endregion
+
+interface NumberConstructor {
+    iota<T>(length?: T): number[]
+    rotate<T>(array: T[])
+    signum(): number
+    identity(): number
+    negate(): number
+    reciprocal(): number
+    exponential(): number
+    ln(): number
+    pi(): number
+    factorial(): number
+    roll(): number
+    magnitude(): number
+    ceiling(): number
+    floor(): number
+    same(): number
+}
+
+interface ArrayConstructor {
+    ceiling(array: number[]): number[]
+    floor(array: number[]): number[]
+    iota<T>(length: T): number[]
+
+}
+
+interface Array<T> {
+    // signum(): T[]
+    // sigumnProp: T[]
+    primitive(left?, right?): T[]
+    deal(omega: number, alpha?: number): number[]
+    iota(length?: number)
+    plus(omega?): T[]
+    minus(omega?): T[]
+    times(omega?): T[]
+    divide(omega?): T[]
+    minimum(omega?): T[]
+    maximum(omega?): T[]
+    residue(omega?): T[]
+    power(omega?): T[]
+    logarithm(omega?): T[]
+    binomial(omega?): T[]
+    rotate(omega?): T[]
+    slash(omega?): T[]
+    aplReduce(alpha?, omega?): T[]
+
+} 
+
+namespace Monadic {
 
     ///#region "Scalar Monadic"
 
