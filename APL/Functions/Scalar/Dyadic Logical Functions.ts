ed9f9e306d0a7c664dec9f9944b84b2bb53ae790
Louis van Geldrop <louis@vangeldrop.com>
2016-01-10 00:08:32 +0100

    Dyadic Logicl Functions: and function

--- APL/Functions/Scalar/Dyadic Logical Functions.ts
+++ APL/Functions/Scalar/Dyadic Logical Functions.ts
@@ -1,10 +1,12 @@
 ﻿namespace Dyadic {
 
     export namespace Scalar {
+        export var and = (alpha: boolean, omega: boolean) => { return (alpha && omega) }; Boolean.prototype[APLPrefix + 'and'] = function (omega) { return (this.valueOf() && omega) }
 
     }
 
     export namespace Vector {
+
 
     }
 
