07c1fc1d741666acb4a62f249208ddfa14c008ed
Louis van Geldrop <louis@vangeldrop.com>
2016-02-03 18:01:05 +0100

    dubbele primitive weg

--- APL/APLProlog.ts
+++ APL/APLProlog.ts
@@ -19,65 +19,6 @@ var primitive = function (omega, primitive?) {  // Toekennen aan var is Xx snell
     else {
         if (Array.isArray(this)) {
             if (Array.isArray(omega)) {
-                //TODO max en isArray(this)
-                max = Math.min(this.length, omega.length),
-                    results = new Array(max);
-                for (counter = 0; counter < max; counter++) {
-                    results[counter] = primitive(this[counter], omega[counter])
-                }
-                return results
-            } else {
-                max = this.length
-                results = new Array(max);
-                for (counter = 0; counter < max; counter++) {
-                    results[counter] = primitive(this[counter], omega.valueOf())
-                }
-                return results
-            }
-        } else {
-            if (Array.isArray(omega)) {
-                //TODO max en isArray(this)
-                max = omega.length,
-                    results = new Array(max);
-                for (counter = 0; counter < max; counter++) {
-                    results[counter] = primitive(this.valueOf(), omega[counter])
-                }
-                return results
-            } else {
-                return primitive(this.valueOf(), omega.valueOf())
-            }
-        }
-    }
-    
-            
-    //}
-    //catch (error) {
-    //   // throw error
-    //}
-    //finally {
-    //}
-
-}
-
-var primitive = function (omega, primitive?) {  // Toekennen aan var is Xx sneller dan pure declaratie "function primitive(...) {}" .Uitkijken met lambda. "this." klopt niet meer 
-    // tenzij je het volgende  doet: https://github.com/Microsoft/TypeScript-Handbook/blob/master/pages/Functions.md
-            
-    //try {                                 // Try..catch maakt primitive ongeveer 4x langzamer
-    let counter: number,
-        max: number,
-        results
-    if (typeof primitive === 'undefined') {  // omega is nu de primitive functie
-        primitive = omega
-        max = this.length
-        results = new Array(max)
-        for (counter = 0; counter < max; counter++) {
-            results[counter] = primitive(this[counter])
-        }
-        return results
-    }
-    else {
-        if (Array.isArray(this)) {
-            if (Array.isArray(omega)) {
                 max = Math.min(this.length, omega.length),
                     results = new Array(max);
                 for (counter = 0; counter < max; counter++) {
