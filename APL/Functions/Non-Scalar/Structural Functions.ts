47a70c198fb561afa1f9ba9377b787b19fc76812
Louis van Geldrop <louis@vangeldrop.com>
2016-02-21 22:02:47 +0100

    Cosmetica

--- APL/Functions/Non-Scalar/Structural Functions.ts
+++ APL/Functions/Non-Scalar/Structural Functions.ts
@@ -5,33 +5,27 @@
         export var depth = (alpha): number=> {
             let _depth = (alpha: number, omega) => Math.max(alpha, omega.depth)      //.maximum(omega.depth) 
             return Array.isArray(alpha) ? 1 + alpha.reduce(_depth, 0) : 0
-        }
-
-        addProperty([Array, Boolean, Date, Number, String], 'depth', depth, false)
+        }; addProperty([Array, Boolean, Date, Number, String], 'depth', depth, false)
 
         export var depthLength = (alpha): number => {
             let count = 0
-            let _length = alpha => { Array.isArray(alpha) ? alpha.map(y => { _length(y) }) : count += 1 }
+            let _length = alpha => { Array.isArray(alpha) ? alpha.map(alpha => { _length(alpha) }) : count += 1 }
             _length(alpha)
             return count
-        }
-
-        addProperty([Array, Boolean, Date, Number, String], 'depthLength', depthLength, false)
-
-        export var ravel = (alpha) => { return (Array.isArray(alpha) ? alpha : [alpha]) }; addProperty([Array, Boolean, Date, Number, String], "ravel", ravel)
-
-        export var reverse = (alpha) => { return alpha.reverse() }; addProperty([Array, Number, String], "reversed", reverse, false)
-
-        export var shape = (alpha): number[] => { return alpha.length }; addProperty([Array, Number, String], "shape", shape, false)
+        }; addProperty([Array, Boolean, Date, Number, String], 'depthLength', depthLength, false)
 
         export var enlist = (alpha) => {
             let result = []
             let _enlist = (alpha, omega) => { Array.isArray(alpha) ? alpha.map((y) => { _enlist(y, omega) }) : omega.push(alpha) }
             _enlist(alpha, result)
             return result
-        }
+        }; addProperty([Array, Number, String], "enlist", enlist, false)
 
-        addProperty([Array, Number, String], "enlist", enlist, false)
+        export var ravel = (alpha) => { return (Array.isArray(alpha) ? alpha : [alpha]) }; addProperty([Array, Boolean, Date, Number, String], "ravel", ravel)
+
+        export var reverse = (alpha) => { return alpha.reverse() }; addProperty([Array, Number, String], "reversed", reverse, false)
+
+        export var shape = (alpha): number[] => { return alpha.length }; addProperty([Array, Number, String], "shape", shape, false)
 
     }
 
