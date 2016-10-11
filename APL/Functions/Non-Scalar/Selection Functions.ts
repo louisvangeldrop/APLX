9a3da48f9bd72e96b3e8bf291f7e41bece0b1493
Louis van Geldrop <louis@vangeldrop.com>
2016-10-11 21:57:31 +0200

    dyalog.chm erbij

--- APL/Functions/Non-Scalar/Selection Functions.ts
+++ APL/Functions/Non-Scalar/Selection Functions.ts
@@ -4,9 +4,6 @@
 
         export var identity = (alpha) => alpha
 
-
-
-
         addProperty([Array, Boolean, Date, Number, String], "identity", NonScalar.identity, false)
         addProperty([Array, Boolean, Date, Number, String], "same", NonScalar.identity, false)
     }
