58ea182de8e8de8fc75605533055d28d6f677441
louisvangeldrop <louis@vangeldrop.com>
2024-02-06 22:00:14 +0100

    outfile = appwasm.js

---
+++ APLWASM/appWASM.d.ts
@@ -0,0 +1,16 @@
+declare var APLPrefix: string;
+declare function primitive(omega: any, primitive: any): any;
+declare function addPrototype(object: any, name: any, func: any): void;
+declare function addProperty(object: any, name: any, func: any, primitive?: boolean): void;
+declare var prefix: string;
+declare var Monadic: any;
+declare var Dyadic: any;
+declare var process: any;
+declare var logs: string[];
+declare class APLXTest {
+    start(): void;
+    stop(): void;
+}
+declare var showPerformanceWeb: (spanCPU: HTMLElement, performanceNow: any, text: string, expression: any) => void;
+declare var showPerformance: (performanceNow: number, text: string, fun: any) => number;
+declare var greeter: APLXTest;
